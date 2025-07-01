bd78b4f35cd11980a290c5ca50403bb63491df78
shahad-alrehily <shahadalrehily2@gmail.com>
2025-07-02 01:51:14 +0300

    Create day3.py
    Day 01 solution

---
+++ Day3/day3.py
@@ -0,0 +1,8 @@
+print("MENU")
+food = input("Name a food : ")
+plant = input( "Name a plant : ")
+cooking = input( "Name a method of cooking : ")
+type = input("What word describes Burned food? : ")
+item = input( "Name a DIY item : ")
+
+print(cooking ,food,"with", type ,plant ,"on a bed of", item)
