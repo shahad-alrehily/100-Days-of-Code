c76fc6a4aa8c4ed483ae97503625f711c192b984
shahad-alrehily <shahadalrehily2@gmail.com>
2025-07-02 01:59:27 +0300

    Create day5.py

---
+++ Day5/day5.py
@@ -0,0 +1,16 @@
+print("Marvel Movie Character Creator")
+
+spiderman = input("Do you like 'hanging around'?: ")
+korg = input("Do you have a 'gravelly' voice?: ")
+captain = input("Do you often feel 'Marvelous'?: ")
+
+if spiderman == "yes":
+  print("WOO,you're a spiderman!")
+elif korg == "yes":
+  print("Awesome, you're a korg!")
+elif captain == "yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("ok, it's fine!")
+
+
